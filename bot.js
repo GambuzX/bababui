a0150159d1ac138b0cde83afa1b28b6d6bbc7c63
GambuzX <mg_mesquita@hotmail.com>
2020-08-09 21:00:38 +0100

    updated dependencies

--- bot.js
+++ bot.js
@@ -1,28 +1,33 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+
+/*const Discord = require('discord.io');
+const logger = require('winston');
+const auth = require('./auth.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
 // Initialize Discord Bot
-var bot = new Discord.Client({
+const bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+        let args = message.substring(1).split(' ');
+        const cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
@@ -36,4 +41,22 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             // Just add any case commands if you want to..
          }
      }
+});*/
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(auth.token);
+
+// https://discord.js.org/#/docs/main/stable/general/welcome
